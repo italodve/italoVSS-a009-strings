61547158ecfddd3fe6857b210793034a9aa2e0ac
italodve <italovinicius@Gmail.com>
2022-08-31 15:48:47 -0300

    upei

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,5 @@
 const nome = prompt('qual o seu nome')  //a
 const comida1 = prompt("qual sua comida favorita")
-const comida2 = prompt("qual sua comida favorita")
+const comida2 = prompt("qual sua comida favorita")   //b
 const comida3 = prompt("qual sua comida favorita")   
-console.log(`essas são suas comidas favoritas ${nome} \n -${comida1} \n -${comida2} \n -${comida3}`)
+console.log(`essas são suas comidas favoritas ${nome} \n -${comida1} \n -${comida2} \n -${comida3}`) 
